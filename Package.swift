af1ad10cf8bb714ffe3b8c1b08ad47f1108f4d9d
thanhtan541 <thanhtan004100@gmail.com>
2025-07-23 16:16:16 +0700

    feat: rename package

--- Package.swift
+++ Package.swift
@@ -3,27 +3,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "SpruceIDMobileSdk",
+    name: "MobileSdkRs",
     platforms: [
         .iOS(.v15)
     ],
     products: [
         .library(
-            name: "SpruceIDMobileSdk",
-            targets: ["SpruceIDMobileSdk"]
-        ),
-        .library(
-            name: "SpruceIDMobileSdkRs",
-            targets: ["SpruceIDMobileSdkRs"]
-        ),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-algorithms", from: "1.2.0")
+            name: "MobileSdkRs",
+            targets: ["MobileSdkRs"]
+        )
     ],
     targets: [
         .binaryTarget(name: "RustFramework", url: "https://github.com/tdog-space/rust-sdk/releases/download/0.1.0/RustFramework.xcframework.zip", checksum: "32116dd9a96485563b67b64f48544c5e31f99c564dd9ba8e7774370ffaba2101"),
         .target(
-            name: "SpruceIDMobileSdkRs",
+            name: "MobileSdkRs",
             dependencies: [
                 .target(name: "RustFramework")
             ],
@@ -33,20 +26,10 @@ let package = Package(
             ]
         ),
         .target(
-            name: "SpruceIDMobileSdk",
-            dependencies: [
-                .target(name: "SpruceIDMobileSdkRs"),
-                .product(name: "Algorithms", package: "swift-algorithms"),
-            ],
             path: "./ios/MobileSdk/Sources/MobileSdk",
             swiftSettings: [
                 //.swiftLanguageMode(.v5)  // some of our code isn't concurrent-safe (e.g. OID4VCI.swift)
             ]
-        ),
-        .testTarget(
-            name: "SpruceIDMobileSdkTests",
-            dependencies: ["SpruceIDMobileSdk"],
-            path: "./ios/MobileSdk/Tests/MobileSdkTests"
         ),
     ]
 )
